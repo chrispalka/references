ada4792fe3585192bd431d3c7c3ec33c2d31ec2b
Rails Udemy Course <railsudemycourse@gmail.com>
2019-01-16 19:29:30 -0500

    Add Wheel of Fortune game

---
+++ wheel_of_fortune_regex.rb
@@ -0,0 +1,24 @@
+starting_sentence = "Hi from matching land"
+sentence_array = starting_sentence.split("").map(&:downcase)
+accurate_count = sentence_array - [" "]
+final_sentence = starting_sentence.gsub(/[a-zA-Z]/, "_").split("")
+
+while sentence_array.count("") < accurate_count.count
+     puts "Guess a letter:"
+     guess = gets.downcase.chomp
+     
+     if sentence_array.include?(guess)
+        letter_index = sentence_array.find_index(guess)
+        sentence_array[letter_index] = ""
+        final_sentence[letter_index] = guess
+        puts "Correct! The sentence is now: #{final_sentence.join}"
+     else
+        puts "Sorry, that letter is not correct. Please try again."
+     end
+     
+end
+
+if sentence_array.count("") == accurate_count.count
+        puts "Congratulations! You win!"
+end
+
